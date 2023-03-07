7fe1794bca72de746c2d01b9007c960cc1e65315
Boye Sjo <boyegs@gmail.com>
2023-03-07 10:36:25 +0100

    new test

--- code/wan/results/conv.py
+++ code/wan/results/conv.py
@@ -1,22 +1,31 @@
-# %%
+import glob
 from pathlib import Path
 
 import pandas as pd
+from loguru import logger
 
-folder = "easy"
-prefix = "thompson"
+folder = "low_prob_fix"
+prefixes = [
+    "thompson",
+    "ucb",
+    "qucb",
+]
 
-# %%
-df_list = []
-for i in range(100):
-    df = pd.read_csv(Path() / folder / f"{prefix}_{i}.csv")
-    df["turn"] = df.index
-    df["sim"] = i
-    df.set_index(["turn", "sim"], inplace=True)
-    df_list.append(df)
+for prefix in prefixes:
+    df_list = []
+    paths = glob.glob(str(Path() / folder / f"{prefix}_*.csv"))
+    for path in paths:
+        logger.info(path)
+        df = pd.read_csv(path)
+        df["turn"] = df.index
+        df["sim"] = path.split("_")[-1].split(".")[0]
+        df.set_index(["turn", "sim"], inplace=True)
+        df_list.append(df)
 
-df = pd.concat(df_list)
-# %%
-df.reset_index().to_parquet(
-    Path() / folder / f"{prefix}.parquet", index=False, compression="brotli"
-)
+    df = pd.concat(df_list)
+    df.reset_index().to_parquet(
+        Path() / folder / f"{prefix}.parquet",
+        index=False,
+        compression="brotli",
+    )
+    logger.success(f"{prefix} done")
