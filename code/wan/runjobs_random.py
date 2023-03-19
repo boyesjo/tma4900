8e4a81e771ccea53b063883e29c32b44ac9da272
Boye Sjo <boyegs@gmail.com>
2023-03-19 20:14:55 +0100

    add second bayesian experiment

--- code/wan/runjobs_random.py
+++ code/wan/runjobs_random.py
@@ -6,12 +6,13 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from classical import run_thompson, run_ucb
+from new_classical import run_thompson, run_ucb
 from loguru import logger
 from qucb1 import QUCB1
+from scipy.stats import beta, norm
 
 settings = {
-    "folder": "random",
+    "folder": "random2",
     "horizon": 250_000,
     "delta": 0.01,
     "n_simulations": 1000,
@@ -19,38 +20,52 @@ settings = {
 
 
 def run_qucb1(
-    filename: str,
-    folder: str,
+    name: str,
     p_list: np.ndarray,
     horizon: int,
     delta: float,
-) -> None:
-    logger.info(f"{filename} started")
+) -> pd.DataFrame:
+    logger.info(f"{name} started")
     qucb1 = QUCB1(p_list, C1=2)
     arms_played, times_played = qucb1.run(horizon, delta)
-    logger.success(f"{filename} finished")
-    pd.DataFrame({"regret": np.cumsum(qucb1.regret()[:horizon])}).to_csv(
-        Path("results") / folder / f"{filename}.csv",
-        index=False,
-    )
+    logger.success(f"{name} finished")
+    df = pd.DataFrame({"regret": np.cumsum(qucb1.regret()[:horizon])})
+    return df
+
+
+def gen_p_list():
+    a = 0.5
+    b = 0.5
+    sigma = 0.1
+    p1 = beta.rvs(a, b)
+    logit_p1 = np.log(p1 / (1 - p1))
+    logit_p2 = logit_p1 + norm.rvs(scale=sigma)
+    p2 = 1 / (1 + np.exp(-logit_p2))
+    return np.array([p1, p2])
 
 
 def run_all(
+    p_list: np.ndarray,
     sim: int,
-    folder: str,
+    folder: Path,
     horizon: int,
     delta: float = 0.01,
 ) -> None:
     # kys unix
     np.random.seed((os.getpid() * int(time.time())) % 123456789)
-    p_list = np.random.uniform(size=2)
     logger.info(f"{sim=}, {p_list=}")
 
-    p_list.tofile(Path("results") / folder / f"p_list_{sim}.csv", sep=",")
+    df_qucb = run_qucb1(f"qucb_{sim}", p_list, horizon, delta)
+    df_thompson = run_thompson(f"thompson_{sim}", p_list, horizon)
+    df_ucb = run_ucb(f"ucb_{sim}", p_list, horizon)
 
-    run_qucb1(f"qucb_{sim}", folder, p_list, horizon, delta)
-    run_thompson(f"thompson_{sim}", folder, p_list, horizon)
-    run_ucb(f"ucb_{sim}", folder, p_list, horizon)
+    # join and rename columns
+    df = pd.concat([df_qucb, df_thompson, df_ucb], axis=1)
+    df.columns = ["qucb", "thompson", "ucb"]
+    df.to_parquet(
+        folder / f"sim_{sim}.parquet", index=False, compression="brotli"
+    )
+    logger.success(f"{sim=} finished and saved to disk. {p_list=}")
 
 
 if __name__ == "__main__":
@@ -64,14 +79,24 @@ if __name__ == "__main__":
     with open(folder / "settings.txt", "w") as f:
         f.write(str(settings))
 
+    # generate p_lists
+    df_p_list = pd.DataFrame(
+        [gen_p_list() for _ in range(settings["n_simulations"])],
+        columns=["p1", "p2"],
+    )
+    df_p_list.to_parquet(
+        folder / "p_list.parquet", index=False, compression="brotli"
+    )
+
     tasks = np.arange(settings["n_simulations"])
     with multiprocessing.Pool() as pool:
         pool.starmap(
             run_all,
             [
                 (
+                    df_p_list.iloc[sim].values,
                     sim,
-                    settings["folder"],
+                    folder,
                     settings["horizon"],
                     settings["delta"],
                 )
