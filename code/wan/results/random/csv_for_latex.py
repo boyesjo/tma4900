821bf29906107c97624641442c14a0e1f0fb1991
Boye Sjo <boyegs@gmail.com>
2023-04-30 20:02:08 +0200

    add more sims

--- code/wan/results/random/csv_for_latex.py
+++ code/wan/results/random/csv_for_latex.py
@@ -9,7 +9,11 @@ FOLDER = Path()
 # %%
 df_qucb = pd.read_parquet(FOLDER / "qucb.parquet").set_index("turn")
 df_ucb = pd.read_parquet(FOLDER / "ucb.parquet").set_index("turn")
-df_thomp = pd.read_parquet(FOLDER / "thomp.parquet").set_index("turn")
+df_thomp = (
+    pd.read_parquet(FOLDER / "thomp.parquet")
+    .set_index("turn")
+    .rename({"thompson": "thomp"}, axis=1)
+)
 
 HORIZON = df_qucb.index.max() + 1
 
@@ -32,13 +36,13 @@ for df, name in [
 ]:
     # grab 200 turns evenly spaced
     df = df.iloc[:: int(HORIZON / 200)]
-    df_all[f"{name}_mean"] = df.groupby("turn").regret.mean()
+    df_all[f"{name}_mean"] = df.groupby("turn")[name].mean()
 
-df_all.to_csv(FOLDER / "plot_data.csv")
+df_all.to_csv(FOLDER / "plot_data.dat")
 
 # %%
 # same but turns spaced on log scale
-turns = np.geomspace(1, HORIZON - 1, 20000).astype(int)
+turns = np.geomspace(1, HORIZON - 1, 200).astype(int)
 
 # if duplicate turns, set to higest not in list
 for i in range(1, len(turns)):
@@ -53,9 +57,9 @@ for df, name in [
     (df_thomp, "thomp"),
 ]:
     df = df.loc[turns]
-    df_all[f"{name}_mean"] = df.groupby("turn").regret.mean()
+    df_all[f"{name}_mean"] = df.groupby("turn")[name].mean()
 
-# df_all.to_csv(FOLDER / "plot_data_log.csv")
+df_all.to_csv(FOLDER / "plot_data_log.dat")
 
 # %%
 df_all.plot(loglog=True)
